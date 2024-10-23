8521044b310cb5076103706b975b036e785c6b38
whgurrb <hgcho3@naver.com>
2024-10-23 16:45:05 +0900

    back-end 10-23 commit

--- src/main/java/com/pipa/back/repository/UserRepository.java
+++ src/main/java/com/pipa/back/repository/UserRepository.java
@@ -7,5 +7,7 @@ import com.pipa.back.entity.UserEntity;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
+
+    UserEntity findByUserId(String userId);
     
 }
