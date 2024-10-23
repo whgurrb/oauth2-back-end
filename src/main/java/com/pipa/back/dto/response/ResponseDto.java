caf30aaa181556e085d99106947effaeb60f6b2d
whgurrb <hgcho3@naver.com>
2024-10-23 13:14:12 +0900

    back-end 10-23 commit

---
+++ src/main/java/com/pipa/back/dto/response/ResponseDto.java
@@ -0,0 +1,13 @@
+package com.pipa.back.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResponseDto {
+    
+    private String code;
+    private String message ;
+    
+}
