0e5b3a15bb565b02749086f3289cbc7268cdf0a4
whgurrb <hgcho3@naver.com>
2024-10-23 14:23:47 +0900

    back-end 10-23 commit

---
+++ src/main/java/com/pipa/back/common/ResponseMessage.java
@@ -0,0 +1,13 @@
+package com.pipa.back.common;
+
+public interface ResponseMessage {
+    String SUCCESS = "Success.";
+
+    String VALIDATION_FAIL = "Validation failed";
+    String DUPLICATE_ID = "Duplication id.";
+
+    String SIGN_IN_FAIL = "Login information mismatched.";
+    String CERTIFICATION_FAIL = "Certification failed.";
+    
+    String DATABASE_ERROR = "Database error.";
+}
--- src/main/java/com/pipa/back/dto/response/ResponseDto.java
+++ src/main/java/com/pipa/back/dto/response/ResponseDto.java
@@ -1,5 +1,11 @@
 package com.pipa.back.dto.response;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.pipa.back.common.ResponseCode;
+import com.pipa.back.common.ResponseMessage;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,5 +15,24 @@ public class ResponseDto {
     
     private String code;
     private String message ;
+
+    public ResponseDto(){
+        this.code = ResponseCode.SUCCESS;
+        this.message=ResponseMessage.SUCCESS;
+    }
+
+    public ResponseEntity<ResponseDto> databaseError(){
+        ResponseDto responseBody = new ResponseDto(ResponseCode.DATABASE_ERROR, ResponseMessage.DATABASE_ERROR );
+        return ResponseEntity.status( HttpStatus.INTERNAL_SERVER_ERROR ).body( responseBody );
+
+    }
+
+    public ResponseEntity<ResponseDto> validationFail(){
+        ResponseDto responseBody = new ResponseDto(ResponseCode.VALIDATION_FAIL, ResponseMessage.VALIDATION_FAIL );
+        return ResponseEntity.status( HttpStatus.BAD_REQUEST ).body( responseBody );
+
+    }
+
+    
     
 }
